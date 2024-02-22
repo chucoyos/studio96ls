898ca7e1318d02c74bcdd02c3a6e04c44d3efe30
chucoyos <unodev@outlook.com>
2024-02-21 19:28:54 -0600

    Add Service scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_22_004945) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_22_011813) do
   create_table "clients", force: :cascade do |t|
     t.string "name"
     t.string "phone"
     t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "services", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.decimal "price"
+    t.decimal "commission"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
