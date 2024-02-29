b70497fccc970290c80475e4dd9c2dab87377f3e
chucoyos <unodev@outlook.com>
2024-02-28 21:24:39 -0600

    Add client-meeting stylist-meeting relationship

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,5 @@
 class Client < ApplicationRecord
   validates :name, presence: true, length: { minimum: 3, message: "Debe tener al menos 3 caracteres"  }
   validates :phone, presence: true, format: { with: /\A[0-9]{10}\z/, message: "Deben ser 10 digitos" }
+  has_many :meetings, dependent: :destroy
 end
