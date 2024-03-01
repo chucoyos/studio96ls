6548bce1b91db81d1a37a982a5999986de033ac9
chucoyos <unodev@outlook.com>
2024-03-01 11:29:39 -0600

    Add uniqueness to client name

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,5 @@
 class Client < ApplicationRecord
-  validates :name, presence: true, length: { minimum: 3, message: "Debe tener al menos 3 caracteres"  }
+  validates :name, presence: true, length: { minimum: 3, message: "Debe tener al menos 3 caracteres"  }, uniqueness: { case_sensitive: false, message: "Ya existe un cliente con ese nombre" }
   validates :phone, presence: true, format: { with: /\A[0-9]{10}\z/, message: "Deben ser 10 digitos" }
   has_many :meetings, dependent: :destroy
 end
